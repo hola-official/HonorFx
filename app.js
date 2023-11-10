274f6be884c574b9ef952f40f00a18da6a185f58
muhammedola <141998553+muhammedola@users.noreply.github.com>
2023-11-10 02:53:20 +0100

    first commit

--- app.js
+++ app.js
@@ -1,54 +1,58 @@
-// // SearchForm Section code
-// let searchForm = document.querySelector('.search-form');
-// const bgAnimation = document.getElementById('bgAnimation');
+app.js
+// SearchForm Section code
+let searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+}
 
-// const numberOfColorBoxes = 400;
+$(document).ready(function(){
 
-// for (let i = 0; i < numberOfColorBoxes; i++) {
-//     const colorBox = document.createElement('div');
-//     colorBox.classList.add('colorbox');
-//     bgAnimation.append(colorBox);
-// }
+    $(window).scroll(function(){
+        //Navbar-bottom scrolling
+        if(this.scrollY > 5){
+            $('.navbar-bottom').addClass("sticky");
+        }else{
+            $('.navbar-bottom').removeClass("sticky");
+        }
 
-const toggleSearchForm = () => searchForm.classList.toggle('active');
-const removeActiveClass = () => searchForm.classList.remove('active');
-
-document.getElementById('search-btn').onclick = toggleSearchForm;
-window.onscroll = removeActiveClass;
-
-document.addEventListener('DOMContentLoaded', function () {
-    window.addEventListener('scroll', function () {
-        const navbarBottom = document.querySelector('.navbar-bottom');
-        const scrollUpBtn = document.querySelector('.scroll-up-btn');
-
-        // Navbar-bottom scrolling
-        navbarBottom.classList.toggle('sticky', this.scrollY > 5);
-
-        // Scrolling Button Btn
-        scrollUpBtn.classList.toggle('show', this.scrollY > 500);
+        //Scrolling Button Btn
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
 
-    // Scroll up script
-    document.querySelector('.scroll-up-btn').addEventListener('click', () => {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-    });
-
-    // Owl Carousel
-    const owlCarousel = document.querySelector('.owl-carousel');
-    if (owlCarousel) {
-        new OwlCarousel(owlCarousel, {
-            margin: 5,
-            navigation: true,
-            loop: true,
-            autoplay: true,
-            autoplayTimeout: 2000,
-            autoplayHoverPause: true,
-
-            responsive: {
-                0: { items: 1, nav: false },
-                600: { items: 2, nav: false },
-                1000: { items: 3, nav: false },
-            },
+        //slide up script
+        $('.scroll-up-btn').click(function(){
+            $('html').animate({scrollTop:0});
         });
-    }
+        
+    //owl owlCarousel
+    $('.owl-carousel').owlCarousel({
+        margin: 5,
+        mavigation: true,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
